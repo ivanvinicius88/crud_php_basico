8ef7e5cfe85d19af536181673b2fe560b0bdd785
Marcondes Maçaneiro <marcondesmacaneiro@gmail.com>
2018-03-28 21:03:24 -0300

    Roteiro para deletar produtos.

--- produto-database.php
+++ produto-database.php
@@ -17,3 +17,19 @@
         return mysqli_query($conexao, $query);
     }
 
+    function buscaProduto($conexao, $id) {
+        $query = "select p.*, 
+                    c.nome as categoria_nome 
+                    from produtos as p
+                    inner join categorias as c
+                    on c.id = p.categoria_id
+                    where p.id = {$id}";
+        $resultado = mysqli_query($conexao, $query);
+        return mysqli_fetch_assoc($resultado);
+    }
+
+    function removeProduto($conexao, $id) {
+        $query = "delete from produtos where id = {$id}";
+        return mysqli_query($conexao, $query);
+    }
+
